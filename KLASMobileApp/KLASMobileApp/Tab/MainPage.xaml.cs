03e24e326d3a38c80abb6e47bd8079b3ad3018b2
LimMoong <chk705@gmail.com>
2020-06-23 15:02:59 +0900

    fix conflict

--- KLASMobileApp/KLASMobileApp/Tab/MainPage.xaml.cs
+++ KLASMobileApp/KLASMobileApp/Tab/MainPage.xaml.cs
@@ -28,43 +28,25 @@ namespace KLASMobileApp.Tab
             BindingContext = new ExamVM { Exams = exams };
 
             webView.Source = "https://klas.kw.ac.kr/std/cmn/frame/Frame.do";
-
-
-            WebView a = new WebView();
-            
-           
-           
-
-
         }
 
         protected async override void OnAppearing()
         {
             base.OnAppearing();
-<<<<<<< HEAD
-            //label.Text = "테스트 api 호출 : "+ await App.RestManager.GetSchdulInfo("2020,1", "U202018485H030023");
-=======
->>>>>>> d32127fd8b55c5557d31183628e316f6352c52d9
 
             SyllabusSearchInfo syllabusSearchInfo = new SyllabusSearchInfo();
             syllabusSearchInfo.Year = 2020;
             syllabusSearchInfo.Semester = 1;
             syllabusSearchInfo.IsMyLecture = true;
 
-<<<<<<< HEAD
             App.RestManager.getCookies();
 
-
-
-            Console.WriteLine(str);
-=======
             List<SyllabusInfo> syllabusInfos = await App.RestManager.SearchSyllabus(syllabusSearchInfo);
 
             foreach (var v in syllabusInfos)
             {
                 Debug.Print("{0}\t{1}\t{2}", v.LectureName, v.LectureType, v.ProfessorName);
             }
->>>>>>> d32127fd8b55c5557d31183628e316f6352c52d9
         }
     }
 
