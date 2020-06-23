72698ae5b6c9dfc741997c1e3aae30d3eb1b2679
retn0 <nbsp1221@gmail.com>
2020-06-24 05:08:50 +0900

    [UPDATE] 푸시 알림에 중복 체크 여부 추가
    푸시 알림을 추가하는 AddNotification 함수에 중복 여부를 체크할 수 있는
    데이터를 보낼 수 있도록 만들었습니다.

--- KLASMobileApp/KLASMobileApp/Data/Notification.cs
+++ KLASMobileApp/KLASMobileApp/Data/Notification.cs
@@ -7,6 +7,7 @@ namespace KLASMobileApp.Data
 {
     public class NotificationInfo
     {
+        public string DuplicateCheck { get; set; }
         public string StudentCode { get; set; }
         public string NotificationTime { get; set; }
         public string MessageTitle { get; set; }
@@ -14,22 +15,25 @@ namespace KLASMobileApp.Data
 
         public NotificationInfo()
         {
+            this.DuplicateCheck = "";
             this.StudentCode = "";
             this.NotificationTime = "";
             this.MessageTitle = "";
             this.MessageBody = "";
         }
 
-        public NotificationInfo(string studentCode, string notificationTime, string messageTitle, string messageBody)
+        public NotificationInfo(string duplicateCheck, string studentCode, string notificationTime, string messageTitle, string messageBody)
         {
+            this.DuplicateCheck = duplicateCheck;
             this.StudentCode = studentCode;
             this.NotificationTime = notificationTime;
             this.MessageTitle = messageTitle;
             this.MessageBody = messageBody;
         }
 
-        public NotificationInfo(string studentCode, DateTime notificationTime, string messageTitle, string messageBody)
+        public NotificationInfo(string duplicateCheck, string studentCode, DateTime notificationTime, string messageTitle, string messageBody)
         {
+            this.DuplicateCheck = duplicateCheck;
             this.StudentCode = studentCode;
             this.NotificationTime = ConvertDateTime(notificationTime);
             this.MessageTitle = messageTitle;
@@ -50,6 +54,7 @@ namespace KLASMobileApp.Data
         {
             return JsonConvert.SerializeObject(new
             {
+                duplicateCheck = this.DuplicateCheck,
                 studentCode = this.StudentCode,
                 notificationTime = this.NotificationTime,
                 messageTitle = this.MessageTitle,
